910dc02cd3c9caf968a1e0c2117debfdcfe08e0c
vladysik03 <zhurkina56@gmail.com>
2025-06-04 13:16:32 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,37 @@
+import { defineConfig } from 'vite'
+import pugPlugin from 'vite-plugin-pug'
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath
+
+ } from 'url';
+const options = { pretty: true }
+const locals = { name: 'Flavien' }
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [pugPlugin(options, locals)],
+  base: './', // Критически важно для работы без сервера
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets', // Папка для ресурсов (css, images)
+    emptyOutDir: true, // Очищать папку при каждой сборке
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]', // Формат имен файлов
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src') // Пример алиаса (если нужно)
+    }
+  },
+  server: {
+    cors: true
+  },
+  preview: {
+    cors: true
+  }
+});
